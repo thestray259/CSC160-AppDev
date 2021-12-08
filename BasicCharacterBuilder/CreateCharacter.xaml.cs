e9140df5a50f5b63c29dee7baf0d27e2692524dd
Tobie Allen <tallen@student.neumont.edu>
2021-12-08 10:28:51 -0700

    Create character roughly finished

--- BasicCharacterBuilder/CreateCharacter.xaml.cs
+++ BasicCharacterBuilder/CreateCharacter.xaml.cs
@@ -30,23 +30,23 @@ namespace BasicCharacterBuilder
         private CharactersTableAdapters.tblCharacterTableAdapter charTable = new CharactersTableAdapters.tblCharacterTableAdapter();
         private Characters chars = new Characters();
 
-        Random random = new Random(); 
-        int randName;
-        int randAge;
-        int randRace;
-        int randClass; 
+        Random random = new Random();
+        private int randName;
+        private int randAge;
+        private int randRace;
+        private int randClass;
 
-        string name; 
-        string age;
-        string gender;
-        string race = ""; 
-        string charClass = ""; 
-        int str = 0;
-        int dex = 0;
-        int con = 0;
-        int inte = 0;
-        int wis = 0;
-        int cha = 0; 
+        private string name;
+        private string age;
+        private string gender;
+        private string race = "";
+        private string charClass = "";
+        private int str = 0;
+        private int dex = 0;
+        private int con = 0;
+        private int inte = 0;
+        private int wis = 0;
+        private int cha = 0;
 
         string[] names = new string[] { "Finnley", "Morgan", "Jessie", "Skylar", "Frankie", "Quinn", "Adrian", "Parker", "Arrow", //9
                                           "Arden", "Valen", "Ziv", "Xen", "Vesper", "Sparrow", "Roux", "Reef", "Owen", "Orion", //10
@@ -342,17 +342,126 @@ namespace BasicCharacterBuilder
 
         private void btnCreateF_Click(object sender, RoutedEventArgs e)
         {
+            if (cfName.Text == "") MessageBox.Show("Name cannot be empty. Please try again. ");
+            else if (cfName.Text.Contains((char)48) || cfName.Text.Contains((char)49) || cfName.Text.Contains((char)50) || cfName.Text.Contains((char)51) || cfName.Text.Contains((char)52) || cfName.Text.Contains((char)53) || cfName.Text.Contains((char)54) || cfName.Text.Contains((char)55) || cfName.Text.Contains((char)56) || cfName.Text.Contains((char)57)) MessageBox.Show("Name cannot contain numbers. Please try again.");
+            else if (cfAge.Text != "" && !(cfAge.Text.Contains((char)48) || cfAge.Text.Contains((char)49) || cfAge.Text.Contains((char)50) || cfAge.Text.Contains((char)51) || cfAge.Text.Contains((char)52) || cfAge.Text.Contains((char)53) || cfAge.Text.Contains((char)54) || cfAge.Text.Contains((char)55) || cfAge.Text.Contains((char)56) || cfAge.Text.Contains((char)57))) MessageBox.Show("Age must be a number");
+            else
+            {
+                // set properties specific to male 
+                name = cfName.Text.ToString();
+                if (cfAge.Text == "") age = "Unknown";
+                else age = cfAge.Text.ToString();
+                gender = "Female";
 
+                if (race == "") race = "Human";
+                if (charClass == "") charClass = "Classless";
+
+                // enter information into database 
+                SetInfo();
+            }
         }
 
         private void cfRace_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            if (fHuman.IsHighlighted) race = "Human";
+            if (fDragonborn.IsHighlighted)
+            {
+                race = "Dragonborn";
+                str += 2;
+                cha++;
+            }
+            if (fDwarf.IsHighlighted)
+            {
+                race = "Dwarf";
+                con++;
+            }
+            if (fElf.IsHighlighted)
+            {
+                race = "Elf";
+                dex += 2;
+            }
+            if (fGnome.IsHighlighted)
+            {
+                race = "Gnome";
+                inte++;
+            }
+            if (fHalfling.IsHighlighted)
+            {
+                race = "Halfling";
+                dex += 2;
+            }
+            if (fTiefling.IsHighlighted)
+            {
+                race = "Tiefling";
+                inte++;
+                cha += 2;
+            }
         }
 
         private void cfClass_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            if (fClassless.IsHighlighted) charClass = "Classless";
+            if (fBarbarian.IsHighlighted)
+            {
+                charClass = "Barbarian";
+                str++;
+            }
+            if (fBard.IsHighlighted)
+            {
+                charClass = "Bard";
+                cha++;
+            }
+            if (fCleric.IsHighlighted)
+            {
+                charClass = "Cleric";
+                wis++;
+            }
+            if (fDruid.IsHighlighted)
+            {
+                charClass = "Druid";
+                wis++;
+            }
+            if (fFighter.IsHighlighted)
+            {
+                charClass = "Fighter";
+                str++;
+                dex++;
+            }
+            if (fMonk.IsHighlighted)
+            {
+                charClass = "Monk";
+                dex++;
+            }
+            if (fPaladin.IsHighlighted)
+            {
+                charClass = "Paladin";
+                str++;
+            }
+            if (fRanger.IsHighlighted)
+            {
+                charClass = "Ranger";
+                dex++;
+            }
+            if (fRouge.IsHighlighted)
+            {
+                charClass = "Rouge";
+                dex++;
+            }
+            if (fSorcerer.IsHighlighted)
+            {
+                charClass = "Sorcerer";
+                cha++;
+            }
+            if (fWarlock.IsHighlighted)
+            {
+                charClass = "Warlock";
+                cha++;
+            }
+            if (fWizard.IsHighlighted)
+            {
+                charClass = "Wizard";
+                inte++;
+            }
         }
 
         // Other
@@ -364,17 +473,126 @@ namespace BasicCharacterBuilder
 
         private void btnCreateO_Click(object sender, RoutedEventArgs e)
         {
+            if (coName.Text == "") MessageBox.Show("Name cannot be empty. Please try again. ");
+            else if (coName.Text.Contains((char)48) || coName.Text.Contains((char)49) || coName.Text.Contains((char)50) || coName.Text.Contains((char)51) || coName.Text.Contains((char)52) || coName.Text.Contains((char)53) || coName.Text.Contains((char)54) || coName.Text.Contains((char)55) || coName.Text.Contains((char)56) || coName.Text.Contains((char)57)) MessageBox.Show("Name cannot contain numbers. Please try again.");
+            else if (coAge.Text != "" && !(coAge.Text.Contains((char)48) || coAge.Text.Contains((char)49) || coAge.Text.Contains((char)50) || coAge.Text.Contains((char)51) || coAge.Text.Contains((char)52) || coAge.Text.Contains((char)53) || coAge.Text.Contains((char)54) || coAge.Text.Contains((char)55) || coAge.Text.Contains((char)56) || coAge.Text.Contains((char)57))) MessageBox.Show("Age must be a number");
+            else
+            {
+                // set properties specific to male 
+                name = coName.Text.ToString();
+                if (coAge.Text == "") age = "Unknown";
+                else age = coAge.Text.ToString();
+                gender = "Other";
 
+                if (race == "") race = "Human";
+                if (charClass == "") charClass = "Classless";
+
+                // enter information into database 
+                SetInfo();
+            }
         }
 
         private void coRace_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            if (oHuman.IsHighlighted) race = "Human";
+            if (oDragonborn.IsHighlighted)
+            {
+                race = "Dragonborn";
+                str += 2;
+                cha++;
+            }
+            if (oDwarf.IsHighlighted)
+            {
+                race = "Dwarf";
+                con++;
+            }
+            if (oElf.IsHighlighted)
+            {
+                race = "Elf";
+                dex += 2;
+            }
+            if (oGnome.IsHighlighted)
+            {
+                race = "Gnome";
+                inte++;
+            }
+            if (oHalfling.IsHighlighted)
+            {
+                race = "Halfling";
+                dex += 2;
+            }
+            if (oTiefling.IsHighlighted)
+            {
+                race = "Tiefling";
+                inte++;
+                cha += 2;
+            }
         }
 
         private void coClass_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            if (oClassless.IsHighlighted) charClass = "Classless";
+            if (oBarbarian.IsHighlighted)
+            {
+                charClass = "Barbarian";
+                str++;
+            }
+            if (oBard.IsHighlighted)
+            {
+                charClass = "Bard";
+                cha++;
+            }
+            if (oCleric.IsHighlighted)
+            {
+                charClass = "Cleric";
+                wis++;
+            }
+            if (oDruid.IsHighlighted)
+            {
+                charClass = "Druid";
+                wis++;
+            }
+            if (oFighter.IsHighlighted)
+            {
+                charClass = "Fighter";
+                str++;
+                dex++;
+            }
+            if (oMonk.IsHighlighted)
+            {
+                charClass = "Monk";
+                dex++;
+            }
+            if (oPaladin.IsHighlighted)
+            {
+                charClass = "Paladin";
+                str++;
+            }
+            if (oRanger.IsHighlighted)
+            {
+                charClass = "Ranger";
+                dex++;
+            }
+            if (oRouge.IsHighlighted)
+            {
+                charClass = "Rouge";
+                dex++;
+            }
+            if (oSorcerer.IsHighlighted)
+            {
+                charClass = "Sorcerer";
+                cha++;
+            }
+            if (oWarlock.IsHighlighted)
+            {
+                charClass = "Warlock";
+                cha++;
+            }
+            if (oWizard.IsHighlighted)
+            {
+                charClass = "Wizard";
+                inte++;
+            }
         }
     }
 }
